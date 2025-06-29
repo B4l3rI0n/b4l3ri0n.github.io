00dcc34cb390667880f1b98c7753dad92606d687
C3rB3rUs <c3rb3rus@B4l3rI0n.RedRightHanf.fuck>
2025-06-29 21:15:11 +0000

    repo

--- update-repos.sh
+++ update-repos.sh
@@ -2,7 +2,7 @@
 
 # Fetch the GitHub API data and save it to data/repos.json
 #mkdir -p data
-curl -s 'https://api.github.com/users/B4l3rI0n/repos' | jq . > _data/repos.json
+curl -s 'https://api.github.com/users/B4l3rI0n/repos' | jq . > ~/blog/b4l3ri0n.github.io/_data/repos.json
 
 # Check if the file was created successfully
 if [ -s _data/repos.json ]; then
